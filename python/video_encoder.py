9d8a485c3392a51568c9f1042868d2f315c5de9f
Michelle Rogers <michelle@merogers.dev>
2024-01-04 17:59:44 -0600

    feat: finish video encoding script

--- python/video_encoder.py
+++ python/video_encoder.py
@@ -1,36 +1,84 @@
 #! /usr/bin/env python3
 
+# Version 1.0
+
 import os
 import re
 import subprocess
+from shutil import which
 
-episode_regex='\d{4}'
-movie_regex='\d{4}'
-allowed_extensions = ['mkv', 'mp4']
+def main():
+  episode_regex='\w{1}\d{2}\w{1}\d{2}'
+  year_regex='\d{4}'
+  allowed_extensions = ['mkv', 'mp4']
+  roku_info = ".Roku.1080p.Surround.x264.mp4"
 
-def convert_video(input_file, output_file):
-  ffmpeg_cmd = [
-    "ffmpeg",
-    "-i",
-    input_file,
-    "-vn",
-    "-acodec",
-    "libmp3lame",
-    "-ab",
-    "192k",
-    "-ar",
-    "44100",
-    "-y",
-    output_file
-  ]
+  def encode_video(input_file, output_file):
+    ffmpeg_cmd = [
+      "ffmpeg",
+      "-i",
+      input_file,
+      "-c:v",
+      "libx264",
+      "-preset",
+      "medium",
+      "-crf",
+      "22",
+      "-n",
+      output_file
+    ]
 
-  try:
-    subprocess.run(ffmpeg_cmd, check=True)
-    print("Conversion success")
-  except subprocess.CalledProcessError as e:
-    print("Conversion failed")
+    try:
+      subprocess.run(ffmpeg_cmd, check=True)
+      print(f"Encoding success for {output_file}")
+    except subprocess.CalledProcessError as e:
+      print(f"Encoding failed for {output_file}")
+      print(f"Return Code: {e.returncode}")
+      print(f"Output: {e.output}")
 
-for file in os.listdir('in'):
-  regex = re.search(episode_regex, file)
-  print(file, regex)
+  # Make sure ffmpeg is installed
+  if (which('ffmpeg') is None):
+    print("Cannot find ffmpeg. Please install package.")
+    return
 
+  # Check if input folder exists, else create and exit
+  if not (os.path.exists('in')):
+    os.mkdir('in')
+    print("Input folder found. Creating folder. Please run again.")
+    return
+
+  # Make sure there are files to convert, else exit
+  if(len(os.listdir('in')) == 0):
+    print("No files found to encode. Exiting.")
+    return
+
+  # Create output folder if not found
+  if not (os.path.exists('out')):
+    os.mkdir('out')
+
+  # File List
+  for file in os.listdir('in'):
+    episode = re.search(episode_regex, file)
+    year = re.search(year_regex, file)
+    ext = file[-3:]
+
+    if(episode != None and ext in allowed_extensions):
+      file_segments = re.split(episode_regex, file)
+      new_filename = f"{file_segments[0]}{episode.group()}{roku_info}"
+      if (os.path.isfile(f"./out/{new_filename}")):
+        print(f"./out/{new_filename} already exists. Skipping...")
+        return
+      encode_video(f"./in/{file}", f"./out/{new_filename}")
+    elif(year != None and ext in allowed_extensions):
+      file_segments = re.split(year_regex, file)
+      new_filename = f"{file_segments[0]}{year.group()}{roku_info}"
+      if (os.path.isfile(f"./out/{new_filename}")):
+        print(f"./out/{new_filename} already exists. Skipping...")
+        return
+      encode_video(f"./in/{file}", f"./out/{new_filename}")
+    else:
+      print(file + " is not a valid video file. Skipping...")
+
+# Allows importing as module without executing
+if __name__ == "__main__":
+  main()
