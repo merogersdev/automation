f87948201ef77a560590630829bd2def12bbdb06
Michelle Rogers <michelle@merogers.dev>
2024-01-18 16:07:12 -0600

    feat: add git prompt detection to prompt

--- bash/custom_prompt.sh
+++ bash/custom_prompt.sh
@@ -1,11 +1,31 @@
 #! /usr/bin/bash
 
-# Version 1.0
+# Version 1.1
+
+function check_git_installed {
+  if git --version &>/dev/null;
+  then
+      return 0
+  else
+      return 1
+  fi
+}
+
+function get_git_prompt {
+  line="--------------------------------"
+  if [ ! -f ~/.git-prompt.sh ]; then
+    echo $line
+    echo "Getting Git Prompt"
+    echo $line
+    curl -o ~/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+    echo $line
+    echo "Git Prompt Downloaded"
+    echo $line
+  fi
+  source ~/.git-prompt.sh
+}
 
 function custom_prompt {
-  git --version 2>&1 > /dev/null
-  GIT_IS_AVAILABLE=$?
-
   # Set Colors
   local DIR_COLOR="\[\033[01;34m\]" # Blue
   local GIT_COLOR="\[\033[00;90m\]" # Gray
@@ -24,9 +44,10 @@ function custom_prompt {
   # Trim directory to only 1 level
   export PROMPT_DIRTRIM=1
 
-  #Show prompt with git branch data if git is installed
-  if (( $GIT_IS_AVAILABLE == 0 ))
+  # Show prompt with git branch data if git is installed
+  if check_git_installed -eq 0;
   then
+    get_git_prompt
     export PS1="\n$DIRECTORY $GIT_PROMPT $USER_PROMPT"
   else
     export PS1="\n$DIRECTORY $USER_PROMPT"
