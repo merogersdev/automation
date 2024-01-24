b5e4ce81d5659163b9fd4a38acf867d1f395428b
Michelle Rogers <michelle@merogers.dev>
2024-01-24 16:26:37 -0600

    chore/refactor encoder
    - Move code blocks to functions for better reusability
    - Change to command line arguments for input and output folders
    - Add sorting function to file loop
    - Use os.path.join for concantenating paths instead of .format

--- python/encoder/encoder.py
+++ python/encoder/encoder.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 
-# Version 1.1
+# Version 1.2
 
 import os
 import re
@@ -48,111 +48,88 @@ def encode_video_roku(input_file, output_file):
   "mov_text",
   "-n",
   output_file
-]
+  ]
+
+  # Run ffmpeg encode as Subprocess
+  subprocess.run(ffmpeg_cmd, check=True)
+
+def rename_video(file, encoder_info):
+  episode_regex='\w{1}\d{2}\w{1}\d{2}'
+  episode = re.search(episode_regex, file)
+
+  year_regex='\d{4}'
+  year = re.search(year_regex, file)
+
+  if episode != None:
+    file_segments = re.split(episode_regex, file)
+    return f"{file_segments[0]}{episode.group()}{encoder_info}"
+  file_segments = re.split(year_regex, file)
+  return f"{file_segments[0]}{year.group()}{encoder_info}"
+
+def get_resolution(filename):
+  has_resolution = re.search(r"\.\d{3,4}p\.", filename)
+
+  if has_resolution != None:
+    return has_resolution.group()[1:-1]
+  return "."
+
+def sort_files(directory):
+  with os.scandir(directory) as entries:
+    sorted_entries = sorted(entries, key=lambda entry: entry.name)
+    sorted_items = [entry.name for entry in sorted_entries]
+  return sorted_items
+
+def encode_all_videos_for_roku(in_folder, out_folder):
+  try:
+    for file in sort_files(in_folder):
+      # Skip if it isn't a video file
+      if not file.endswith(('mkv', 'mp4', 'm4v')):
+        print("{} is not a recognised video file. Skipping.".format(file))
+        continue
+
+      resolution = get_resolution(file)
+      new_filename = rename_video(file, ".Roku.{}.Surround.x264.mp4".format(resolution))
+      # Check if file is already encoded
+      if (os.path.exists(os.path.join(out_folder, new_filename))):
+        print("{} already encoded. Skipping".format(file))
+        continue
+      # Encode video
+      encode_video_roku(os.path.join(in_folder,file), os.path.join(out_folder, new_filename))
+      # Cleanup
+      os.remove(os.path.join(in_folder,file))
+      print("Successfully encoded video {}.\n Original file {} deleted.".format(new_filename, file))
+
+  except subprocess.CalledProcessError as e:
+    print("Error Code: ".format(e.returncode), "Details: ".format(e.output))
+    sys.exit(e.returncode)
 
 def main():
-  episode_regex='\w{1}\d{2}\w{1}\d{2}'
-  year_regex='\d{4}'
-  allowed_extensions = {'mkv', 'mp4'}
-  encoder_info = ".Roku.1080p.Surround.x264.mp4"
+  # Check for correct number of arguments
+  if len(sys.argv) != 3:
+    print("Invalid arguments specified.\nUsage: ./encoder.py /in/folder/ /out/folder")
+    sys.exit(1)
 
-  def encode_video(input_file, output_file):
-    # Encode video for Roku Devices
-    # Video: x264 preset medium crf 22
-    # Audio Track 1: aac stereo 128k
-    # Audio Track 2: ac3 5.1 256k
-    # Subtitle Track: mov_text
-    ffmpeg_cmd = [
-      "ffmpeg",
-      "-i",
-      input_file,
-      "-map",
-      "0:0",
-      "-map",
-      "0:1",
-      "-map",
-      "0:1",
-      "-c:v",
-      "libx264",
-      "-preset",
-      "medium",
-      "-crf",
-      "22",
-      "-map",
-      "0:a",
-      "-c:a:0",
-      "aac",
-      "-b:a:0",
-      "128k",
-      "-ac:a:0",
-      "2",
-      "-c:a:1",
-      "ac3",
-      "-b:a:1",
-      "256k",
-      "-ac:a:1",
-      "6",
-      "-c:s",
-      "mov_text",
-      "-n",
-      output_file
-    ]
+  in_folder = sys.argv[1]
+  out_folder = sys.argv[2]
 
-    try:
-      subprocess.run(ffmpeg_cmd, check=True)
-      print(f"Encoding success for {output_file}")
-      #os.remove(f"./in/{file}")
-    except subprocess.CalledProcessError as e:
-      print(f"Encoding failed for {output_file}")
-      print(f"Return Code: {e.returncode}")
-      print(f"Output: {e.output}")
-      os.remove(f"./out/{output_file}")
-      sys.exit(1)
-
-  # Make sure ffmpeg is installed
-  if (which('ffmpeg') is None):
+  # check ffmpeg
+  if which("ffmpeg") is None:
     print("Cannot find ffmpeg. Please install package.")
-    sys.exit(2)
+    sys.exit(1)
 
-  # Check if input folder exists, else create and exit
-  if not (os.path.exists('in')):
-    os.mkdir('in')
-    print("Input folder found. Creating folder. Please add media and run again.")
-    sys.exit(3)
+  # check if in exists and there are files in it
+  if not os.path.exists(in_folder) or not in_folder[:-1] == "/":
+    print("Cannot read from input folder. Please recheck path and include trailing slash.") 
+    sys.exit(1)
 
-  # Make sure there are files to convert, else exit
-  if(len(os.listdir('in')) == 0):
-    print("No files found to encode. Exiting.")
-    sys.exit(4)
+  # check for out folder
+  if not os.path.exists(out_folder) or not out_folder[:-1] == "/":
+    print("Cannot read from output folder. Please recheck path and include trailing slash.") 
+    sys.exit(1)
 
-  # Create output folder if not found
-  if not (os.path.exists('out')):
-    os.mkdir('out')
+  # loop through only video files and encode/rename/output to out dir
+  encode_all_videos_for_roku(in_folder, out_folder)
 
-  # File List
-  for file in os.listdir('in'):
-    episode = re.search(episode_regex, file)
-    year = re.search(year_regex, file)
-    ext = file[-3:]
-
-    if(episode != None and ext in allowed_extensions):
-      file_segments = re.split(episode_regex, file)
-      new_filename = f"{file_segments[0]}{episode.group()}{encoder_info}"
-      if (os.path.isfile(f"./out/{new_filename}")):
-        print(f"./out/{new_filename} already exists. Skipping...")
-      else:
-        encode_video(f"./in/{file}", f"./out/{new_filename}")
-
-    elif(year != None and ext in allowed_extensions):
-      file_segments = re.split(year_regex, file)
-      new_filename = f"{file_segments[0]}{year.group()}{encoder_info}"
-      if (os.path.isfile(f"./out/{new_filename}")):
-        print(f"./out/{new_filename} already exists. Skipping...")
-      else:
-        encode_video(f"./in/{file}", f"./out/{new_filename}")
-        
-    else:
-      print(file + " is not a valid video file. Skipping...")
   sys.exit(0)
 
 # Allows importing as module without executing
